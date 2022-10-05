1a3f450571abc86e05b20194c730165684252178
Paul Robson <paul@robsons.org.uk>
2022-10-05 11:34:31 +0100

    Update

--- src/hardware.asm
+++ src/hardware.asm
@@ -38,7 +38,7 @@ TinyVickyInitialise:
             lda #5
             sta VKY_TXT_CURSOR_Y_REG_L
 
-            lda		#$EC 						; set text colour.
+            lda		#$FC   						; set text colour.
             sta 	TextColour
 
             rts
